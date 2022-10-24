93d56dcd06c56a2c22a3975df8e4523f18989e66
okyueki@gmail.com <okyueki@gmail.com>
2022-10-24 10:46:48 +0700

    perubahan pada profil pegawai

--- application/models/ProfilModel.php
+++ application/models/ProfilModel.php
@@ -3,6 +3,6 @@ class ProfilModel extends CI_model
 {
     public function getAllProfil()
     {
-        return $this->db2->select('pegawai.nik,pegawai.nama,pegawai.tmp_lahir,pegawai.jbtn,departemen.nama as nama_departemen')->join('departemen', 'departemen.dep_id=pegawai.departemen')->get_where('pegawai', ['nik' => $this->session->userdata('nik')])->row_array();
+        return $this->db2->select('pegawai.nik,pegawai.nama,pegawai.tmp_lahir,pegawai.alamat,pegawai.kota,pegawai.jbtn,pegawai.tmp_lahir,pegawai.no_ktp,pegawai.tgl_lahir,departemen.nama as nama_departemen,pegawai.bidang')->join('departemen', 'departemen.dep_id=pegawai.departemen')->get_where('pegawai', ['nik' => $this->session->userdata('nik')])->row_array();
     }
 }
--- application/views/admin/profil.php
+++ application/views/admin/profil.php
@@ -21,18 +21,13 @@
                     <img class="wd-70 rounded-circle" src="https://via.placeholder.com/100x100" alt="profile">
                     <span class="h4 ms-3 text-dark"><?php echo $pegawai['nama']; ?></span>
                   </div>
-                  <div class="d-none d-md-block">
-                    <button class="btn btn-primary btn-icon-text">
-                      <i data-feather="edit" class="btn-icon-prepend"></i> Edit profile
-                    </button>
-                  </div>
                 </div>
               </div>
               <div class="d-flex justify-content-center p-3 rounded-bottom">
                 <ul class="d-flex align-items-center m-0 p-0">
                   <li class="d-flex align-items-center active">
                     <i class="me-1 icon-md text-primary" data-feather="columns"></i>
-                    <a class="pt-1px d-none d-md-block text-primary" href="#">Timeline</a>
+                    <a class="pt-1px d-none d-md-block text-primary" href="#">My Information</a>
                   </li>
                   <li class="ms-3 ps-3 border-start d-flex align-items-center">
                     <i class="me-1 icon-md" data-feather="user"></i>
@@ -73,23 +68,35 @@
                     </div>
                   </div>
                 </div>
-                <p>Hi Saya Pegawai RS Aisyiyah Siti Fatimah</p>
+                <p>Assalamualaikum Saya <?php echo $pegawai['nama']; ?> Pegawai RS Aisyiyah Siti Fatimah Rumah saya beralamat di </p>
                 
                 <div class="mt-3">
-                  <label class="tx-11 fw-bolder mb-0 text-uppercase">Joined:</label>
+                  <label class="tx-11 fw-bolder mb-0 text-uppercase">Nomor Induk Kepegawaian: </label>
                   <p class="text-muted"><?php echo $pegawai['nik']; ?></p>
                 </div>
                 <div class="mt-3">
-                  <label class="tx-11 fw-bolder mb-0 text-uppercase">Lives:</label>
+                  <label class="tx-11 fw-bolder mb-0 text-uppercase">Jabatan:</label>
                   <p class="text-muted"><?php echo $pegawai['jbtn']; ?></p>
                 </div>
                 <div class="mt-3">
-                  <label class="tx-11 fw-bolder mb-0 text-uppercase">Email:</label>
+                  <label class="tx-11 fw-bolder mb-0 text-uppercase">Unit:</label>
                   <p class="text-muted"><?php echo $pegawai['nama_departemen']; ?></p>
                 </div>
                 <div class="mt-3">
-                  <label class="tx-11 fw-bolder mb-0 text-uppercase">Website:</label>
-                  <p class="text-muted">www.nobleui.com</p>
+                  <label class="tx-11 fw-bolder mb-0 text-uppercase">Alamat :</label>
+                  <p class="text-muted"><?php echo $pegawai['alamat']?> <?php echo $pegawai['kota']?></p>
+                </div>
+                <div class="mt-3">
+                  <label class="tx-11 fw-bolder mb-0 text-uppercase">Tempat,Tgl Lahir :</label>
+                  <p class="text-muted"><?php echo $pegawai['tmp_lahir']?>,<?php echo $pegawai['tgl_lahir']?></p>
+                </div>
+                <div class="mt-3">
+                  <label class="tx-11 fw-bolder mb-0 text-uppercase">No KTP :</label>
+                  <p class="text-muted"><?php echo $pegawai['no_ktp']?></p>
+                </div>
+                <div class="mt-3">
+                  <label class="tx-11 fw-bolder mb-0 text-uppercase">Bidang :</label>
+                  <p class="text-muted"><?php echo $pegawai['bidang']?></p>
                 </div>
                 <div class="mt-3 d-flex social-links">
                   <a href="javascript:;" class="btn btn-icon border btn-xs me-2">
